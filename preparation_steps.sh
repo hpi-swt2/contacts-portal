39bd671048be204e164eed593df5b031821e649a
Luc Prestin <l.prestin@gmx.de>
2020-10-23 14:21:43 +0200

    add deletion of related notes if user is deleted, automate the addition of the has_many statement

--- preparation_steps.sh
+++ preparation_steps.sh
@@ -14,7 +14,9 @@ bundle exec rails generate devise:views user
 
 # generate note
 bundle exec rails generate scaffold note title:string content:text user:references
-# manually add to user class: has_many :note
+# use sed to add the line "has_many :notes, dependent: :delete_all" to the user class definition.
+# for a reference on how this statement is cunstructed, see: https://stackoverflow.com/questions/6739258/how-do-i-add-a-line-of-text-to-the-middle-of-a-file-using-bash
+sed -i 'H;${x;s/^\n//;s/end$/  has_many :notes, dependent: :delete_all\n&/;p;}' app/models/user.rb
 
 # migrate database
 bundle exec rails db:migrate
