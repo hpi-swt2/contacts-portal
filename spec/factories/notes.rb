c3ae2e4d85c1ff5e6bc54f199db75fa263f6a3f0
Christoph Matthies <chrisma@users.noreply.github.com>
2020-11-24 12:05:11 +0100

    Bespoke note factory. Ref #6

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :note do
-    title { "Note title" }
-    content { "Note content" }
+    # https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#inline-sequences
+    sequence(:title) { |n| "Note #{n}" }
+    # https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#dependent-attributes
+    content { "The content of #{title.downcase}" }
     # https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#implicit-definition
     user factory: :user
   end
