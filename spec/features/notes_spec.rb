681a1c26a0947d8726f1fa602c637c126ea7b2b3
Christoph Thiede <christoph.thiede@student.hpi.de>
2020-12-04 18:34:58 +0100

    Write acceptance tests 🎉
    Co-authored-by: Alexander Dubrawski <dubrawski.alexander@googlemail.com>

--- spec/features/notes_spec.rb
+++ spec/features/notes_spec.rb
@@ -2,41 +2,43 @@ require 'rails_helper'
 
 describe "Notes", type: :feature do
   before(:each) do
-    @alice = User.new(id: 1, email: "alice@example.com", password: "123")
-    @bob = User.new(id: 2, email: "bob@example.com", password: "456")
+    @alice = User.new(id: 1, email: "alice@example.com", password: "1234567")
+    assert @alice.save
+    @bob = User.new(id: 2, email: "bob@example.com", password: "9876543")
+    assert @bob.save
     login(@alice)
   end
 
   # TODO: Split up
   it "can create a new note" do
-    visit note_path
-    find("a", text: "New Note").click
+    visit notes_path
+    click_link "New Note"
     fill_in "Title", with: "My Title"
     fill_in "Content", with: "My Content"
     fill_in "User", with: @bob.id
-    click "Create Note"
+    click_button "Create Note"
 
-    visit note_path
+    visit notes_path
     expect(page).to have_text("My Title")
     expect(page).to have_text("My Content")
     expect(page).to have_text(@bob.id)
 
     logout
     login(@bob)
-    visit note_path
+    visit notes_path
     expect(page).not_to have_text("My Title")
   end
 
   def login(user)
     visit root_path
-    click "Log-In"
+    click_link "Log-In"
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
-    click "Log in"
+    click_button "Log in"
   end
 
   def logout
     visit root_path
-    click "Sign Out"
+    click_button "Log Out"
   end
 end
