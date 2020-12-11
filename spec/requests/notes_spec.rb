8546f27313d153c909424d52500ddb89a78a588b
Christoph Thiede <christoph.thiede@student.hpi.de>
2020-12-11 18:28:33 +0100

    Adapt failing tests (WIP)
    Co-authored-by: Simon-Pierre Krautkrämer <Simon-Pierre.Krautkraemer@student.hpi.uni-potsdam.de>

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -14,12 +14,26 @@ require 'rails_helper'
 
 RSpec.describe "/notes", type: :request do
   # As you add validations to the `Note` model, be sure to update the factory as well
+  before do
+    @user = FactoryBot.create(:user).id
+    # TODO: Fix test or delete if this is okay
+  end
+
   let(:valid_attributes) {
-    FactoryBot.attributes_for(:note, user_id: FactoryBot.create(:user).id)
+    FactoryBot.attributes_for(
+      :note,
+      user_id: @user,
+      creator_user_id: @user
+    )
   }
 
   let(:invalid_attributes) {
-    FactoryBot.attributes_for(:note, user_id: nil, title: '')
+    FactoryBot.attributes_for(
+      :note,
+      user_id: nil,
+      creator_user_id: nil,
+      title: ''
+    )
   }
 
   describe "GET /index" do
