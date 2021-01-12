ec3e36b2202a3cac302ad3f7ce41687655a43dcf
Paula <paula.klinke@student.hpi.de>
2021-01-12 18:42:22 +0100

    Add failing test for #105
    Co-authored-by: PatriciaSowa <patricia.sowa@student.hpi.de>

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,17 +4,29 @@ RSpec.describe "users/index", type: :view do
   before(:each) do
     @users = assign(:users, FactoryBot.create_list(:user, 3))
     sign_in @users.first
-
-    render
   end
 
   it "renders a list of users" do
+    render
     @users.each do |user|
       expect(rendered).to match user.email
     end
   end
 
-  it "has an + button to add a contact" do
+  it "has an + button to send contact request" do
+    render
     expect(rendered).to have_button('+', count: @users.length)
+  end
+
+  it "does not have a + button for already requested user" do
+    @users.second.contact_requests << @users.first
+    render
+    expect(rendered).not_to have_css('form.button_to[action="/users/2/contact_requests"]')
+  end
+
+  it "does not have a + button for already existing contacts" do
+    @users.first.contacts << @users.second
+    render
+    expect(rendered).not_to have_css('form.button_to[action="/users/2/contact_requests"]')
   end
 end
