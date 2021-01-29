bb05c72143d236f96ba499334404def213bd34e4
Simon-Pierre <simon-pierre.krautkraemer@student.hpi.de>
2021-01-29 17:26:31 +0100

    deleted mobile driver since deprecated

--- spec/features/header_and_footer_spec.rb
+++ spec/features/header_and_footer_spec.rb
@@ -3,14 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Header and Footer', driver: :selenium_headless, type: :feature, js: true do
   describe 'public page without being logged in' do
     before do
-      Capybara.register_driver :iphone do |app| # just testing mobile design
-        require 'selenium/webdriver'
-        profile = Selenium::WebDriver::Firefox::Profile.new
-        profile['general.useragent.override'] = 'iPhone'
-
-        Capybara::Selenium::Driver.new(app, profile: profile)
-      end
-      Capybara.current_driver = :iphone
       visit root_path
     end
 
